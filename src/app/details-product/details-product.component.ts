461dc26a6badd0ccf2031697f97c21b47db30a61
Houdakoubaa <houda.koubaa@esprit.tn>
2022-12-02 15:00:07 +0100

    ActivatedRoute

--- src/app/details-product/details-product.component.ts
+++ src/app/details-product/details-product.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-details-product',
@@ -6,10 +7,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./details-product.component.css']
 })
 export class DetailsProductComponent implements OnInit {
-
-  constructor() { }
+  //récupérer la valeur de l'id envoyé dans l'url
+  //service prédéfini: ActivatedRoute
+   idVal : number;
+  //on injecte le service ActivatedRoute via le constructor
+  constructor(private ac:ActivatedRoute) { }
 
   ngOnInit(): void {
+    //récupérer la valeur du paramètre id 
+    this.idVal=this.ac.snapshot.params['id'];
   }
-
 }
